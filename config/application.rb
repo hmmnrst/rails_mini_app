38a345152a21952fdddca3a1b58fe00b93e54c5d
Masahiro Nomoto <hmmnrst@users.noreply.github.com>
2020-12-22 09:49:37 +0900

    Use `reserve_sql_strategy` option

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'delayed_job_active_record'
 
 class MiniApp < Rails::Application
   config.active_job.queue_adapter = :delayed_job
+  ::Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :default_sql
 
   # config.log_level = :info
   config.log_formatter = ::Logger::Formatter.new
